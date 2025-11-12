57a2889c1d4a439a320a2be398a7951760873aed
DevSushant03 <mrsushant2005@gmail.com>
2025-11-12 18:30:43 +0530

    merge issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()]
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      "3f527d40-b49e-4cc8-94f4-994e247f2842-00-24yksle1zyhdd.sisko.replit.dev",
+    ],
+  },
+});
