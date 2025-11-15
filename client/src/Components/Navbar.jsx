10bf2e31df351dbefae286689425f4b73283ab45
DevSushant03 <mrsushant2005@gmail.com>
2025-11-15 14:41:23 +0530

    modify navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -3,30 +3,42 @@ import { Link } from "react-router-dom";
 import logo from "/public/icon.jpeg";
 function Navbar() {
   return (
-    <nav className="bg-transparent fixed top-0 left-0 w-full z-50">
-      <div className="max-w-7xl mx-auto px-6 py-9 flex justify-between items-center">
+    <nav className="backdrop-blur-sm bg-black/20  fixed top-0 left-0 w-full z-50">
+      <div className="max-w-7xl mx-auto px-6 py-5 flex justify-between items-center">
         {/* Logo */}
         <h2 className="text-2xl font-bold text-orange-600">TaskTribe</h2>
 
         {/* Menu */}
-        <ul className="hidden md:flex gap-8 p-3 px-10 text-white backdrop-blur-sm bg-black/20 rounded-3xl font-medium">
+        <ul className="hidden md:flex gap-8 p-3 px-10 bg-white rounded-3xl font-medium">
           <li>
-            <a href="#home" className="hover:text-orange-600 duration-200">
+            <a
+              href="#home"
+              className="hover:text-white hover:bg-orange-500 rounded-2xl p-2 px-3 duration-200"
+            >
               Home
             </a>
           </li>
           <li>
-            <a href="#process" className="hover:text-orange-600 duration-200">
+            <a
+              href="#process"
+              className="hover:text-white hover:bg-orange-500 rounded-2xl p-2 px-3 duration-200"
+            >
               Features
             </a>
           </li>
           <li>
-            <a href="#about" className="hover:text-orange-600 duration-200">
+            <a
+              href="#about"
+              className="hover:text-white hover:bg-orange-500 rounded-2xl p-2 px-3 duration-200"
+            >
               Category
             </a>
           </li>
           <li>
-            <a href="#contact" className="hover:text-orange-600 duration-200">
+            <a
+              href="#contact"
+              className="hover:text-white hover:bg-orange-500 rounded-2xl p-2 px-3 duration-200"
+            >
               Contact
             </a>
           </li>
