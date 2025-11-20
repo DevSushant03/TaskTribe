df59aeb2b8869d3f0728e9cc1df08537175c1e2a
DevSushant03 <mrsushant2005@gmail.com>
2025-11-21 00:58:46 +0530

    syntax  error

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ import cors from "cors";
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.options("*", cors());
+// app.use(cors());
 app.use(
   cors({
     origin: ["https://tasktribe-plum.vercel.app", "http://localhost:5173"],
